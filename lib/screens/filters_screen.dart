5b98bdcffd7aab3a324aecbefec27d84443481f6
Yassin Rassul <yassinhrassul@gmail.com>
2022-03-24 22:37:38 +0300

    added switch list tiles for the filters screen.

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -10,13 +10,81 @@ class FiltersScreen extends StatefulWidget {
 }
 
 class _FiltersScreenState extends State<FiltersScreen> {
+  var isLactoseFree = false;
+  var isGlutenFree = false;
+  var isVegan = false;
+  var isVegetarian = false;
+
+  Widget buildSwitchListTile({
+    required String theTitle,
+    required String description,
+    required bool currentValue,
+    required Function onChangeHandler,
+  }) {
+    return SwitchListTile(
+      value: currentValue,
+      onChanged: (newValue) => onChangeHandler(newValue),
+      title: Text(theTitle),
+      subtitle: Text(
+        description,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text('Filters!'),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            child: Text(
+              'Adjust the filters for the meals:',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          buildSwitchListTile(
+              theTitle: 'Gluten-free',
+              description: 'only include Gluten-free meals',
+              currentValue: isGlutenFree,
+              onChangeHandler: (newValue) {
+                setState(() {
+                  isGlutenFree = newValue;
+                });
+              }),
+          buildSwitchListTile(
+              theTitle: 'Lactose-free',
+              description: 'only include Lactose-free meals',
+              currentValue: isLactoseFree,
+              onChangeHandler: (newValue) {
+                setState(() {
+                  isLactoseFree = newValue;
+                });
+              }),
+          buildSwitchListTile(
+              theTitle: 'Vegan',
+              description: 'only include Vegan meals',
+              currentValue: isVegan,
+              onChangeHandler: (newValue) {
+                setState(() {
+                  isVegan = newValue;
+                });
+              }),
+          buildSwitchListTile(
+              theTitle: 'Vegetarian',
+              description: 'only include Vegetarian meals',
+              currentValue: isVegetarian,
+              onChangeHandler: (newValue) {
+                setState(() {
+                  isVegetarian = newValue;
+                });
+              }),
+        ],
       ),
     );
   }
